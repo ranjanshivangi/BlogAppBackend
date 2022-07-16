b2267201e519cef715e1391ba8d9dab88c777550
Shivangi Ranjan <shivangiec1098@gmail.com>
2022-07-16 13:58:09 +0530

    [Shivangi] Added: fliter to check if user present

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,15 +1,23 @@
 import bcrypt from "bcrypt";
 
 export const newUser = async (body) => {
-    console.log(body)
-    const collection = require('../config/database')
+  var checkUser = []
+  const collection = require('../config/database')
+  const cursor = await collection.find({ email: body.email });
+  await cursor.forEach(element => {
+    checkUser.push(element)
+  });
+  if (checkUser.length != 0) {
+    throw new Error('User Already Exist')
+  }
+  else {
     const salt = await bcrypt.genSalt(10);
     body.password = await bcrypt.hash(body.password, salt);
-    console.log(body)
     const data = await collection.insertOne({
-        "userName": body.userName,
-        "email": body.email,
-        "password": body.password
+      "userName": body.userName,
+      "email": body.email,
+      "password": body.password
     })
     return data;
+  }
 }
