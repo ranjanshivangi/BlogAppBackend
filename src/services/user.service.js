5ab4e590a1dcf894c18532a801ed301483e22c2f
Shivangi Ranjan <shivangiec1098@gmail.com>
2022-07-19 09:13:34 +0530

    [Shivangi] Refractored: revoved console

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -11,7 +11,7 @@ export const collection = () => {
 export const newUser = async (body) => {
   var checkUser = []
   const { userName, email, password } = body
- 
+
   const cursor = await collection().find({ email: email });
   await cursor.forEach(element => {
     checkUser.push(element)
@@ -30,22 +30,21 @@ export const newUser = async (body) => {
 
 export const login = async (body) => {
   var user = {}
-  const { userName, email, password } = body 
+  const { userName, email, password } = body
   const cursor = await collection().find({ email: email });
   await cursor.forEach(element => {
     user = { ...element }
   });
-   if (user === {}) {
+  if (user === {}) {
     throw new Error('User does not exist with this email')
   }
   else {
     const validPassword = await bcrypt.compare(password, user.password);
     if (validPassword) {
-      let accesstoken = jwt.sign({ email: user.email, id: user._id }, process.env.ACCESS_SECRET_KEY,{ expiresIn: '1m'} );
+      let accesstoken = jwt.sign({ email: user.email, id: user._id }, process.env.ACCESS_SECRET_KEY, { expiresIn: '1m' });
       let refreshToken = jwt.sign({ email: user.email, id: user._id }, process.env.REFRESH_SECRET_KEY);
       tokens[refreshToken] = refreshToken;
-      console.log(tokens)
-      return { user, accesstoken};
+      return { user, accesstoken };
     } else {
       throw new Error("Not a Valid Password");
     }
